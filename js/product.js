75c78c3095313863356068462e31da7c39143464
John Gutierrez <johnegutierrez14@gmail.com>
2024-02-10 21:48:28 -0500

    Add products

--- js/product.js
+++ js/product.js
@@ -1,16 +1,16 @@
 const products = [
     { category: 'pijama', name: 'Pijama Ref. M01', price: '$50.000', image: 'images/Product-M01_0.png' },
-    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_0.JPG' },
-    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_1.JPG' },
+    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_0.jpg' },
+    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_1.jpg' },
     { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_2.png' },
-    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_3.JPG' },
-    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_4.JPG' },
-    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_5.JPG' },
-    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_6.JPG' },
-    // Agrega más productos aquí...
+    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_3.jpg' },
+    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_4.jpg' },
+    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_5.jpg' },
+    { category: 'pijama', name: 'Pijama Ref. M03', price: '$50.000', image: 'images/Product-M03_6.jpg' },
+    { category: 'pijama', name: 'Pijama Ref. M07', price: '$50.000', image: 'images/Product-M07_0.jpg' },
+    { category: 'pijama', name: 'Pijama Ref. M07', price: '$50.000', image: 'images/Product-M07_1.jpg' },
+    { category: 'pijama', name: 'Pijama Ref. M07', price: '$50.000', image: 'images/Product-M07_2.jpg' },
 ];
-
-const productsPerPage = 15;
 
 function generateProductHTML(product) {
     return `
@@ -23,25 +23,24 @@ function generateProductHTML(product) {
                     <div class="block2-txt-child1 flex-col-l ">
                         <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">${product.name}</a>
                         <span class="stext-105 cl3">${product.price}</span>
+                        <div class="size-204 flex-w flex-m respon6-next">
+                          <div class="wrap-num-product flex-w m-r-20 m-tb-10">
+                              <div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
+                                  <i>S</i>
+                              </div>
+                              <div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
+                                  <i>M</i>
+                              </div>
+                              <div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
+                                  <i>L</i>
+                              </div>
+                              <div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
+                                  <i>XL</i>
+                              </div>
+                          </div>
+                      </div>
                     </div>
                 </div>
             </div>
         </div>`;
 }
-
-function renderProducts(pageNumber) {
-    const startIndex = (pageNumber - 1) * productsPerPage;
-    const endIndex = startIndex + productsPerPage;
-    const currentPageProducts = products.slice(startIndex, endIndex);
-
-    const productsContainer = document.getElementById('products-container');
-    productsContainer.innerHTML = '';
-
-    currentPageProducts.forEach(product => {
-        const productHTML = generateProductHTML(product);
-        productsContainer.innerHTML += productHTML;
-    });
-}
-
-// Llamamos a renderProducts con el número de página inicial (p. ej., 1)
-renderProducts(1);
